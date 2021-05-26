dd34cb3f686d74975776542f46191f86953fbab9
sayani2014 <sayanikoley8@gmail.com>
2021-05-26 09:38:17 +0530

    Finding even numbers from 20 to 30 and calculating power of 2 using length of the array UC

--- findLengthofArray.sh
+++ findLengthofArray.sh
@@ -1,1 +1,21 @@
-echo "This is Stage 2 review!"
+#! /bin/bash -x
+
+j=1
+powerofTwo=1
+for((i=20;i<=30;i++))
+do
+	if(($i%2==0))
+	then
+		#echo $i
+		number[$j]=$i
+		j=$(($j+1))
+	fi
+done
+echo "List of even numbers from 20 to 30 in the array: " "${number[*]}"
+
+lengthArray=${#number[*]}
+echo "Length of the array is: " "$lengthArray"
+
+let powerofTwo=(2 ** $lengthArray)
+echo "Power of 2 using the length of the array: $powerofTwo"
+
